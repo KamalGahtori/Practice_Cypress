d846173e0164106928f8b4745146544c6dafac84
Kamal <kamalgahtori22@gmail.com>
2026-02-26 23:21:45 +0530

    added .env config

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
+// cypress.config.js
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
+    supportFile: "cypress/support/e2e.js",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return config;
     },
   },
 });
